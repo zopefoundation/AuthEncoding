142915a909ab852cd3cac3a6894c16718429c425
Michael Howitz <mh@gocept.com>
2020-10-13 08:35:47 +0200

    Update src/AuthEncoding/AuthEncoding.py
    Co-authored-by: Marius Gedminas <marius@gedmin.as>

--- src/AuthEncoding/AuthEncoding.py
+++ src/AuthEncoding/AuthEncoding.py
@@ -74,7 +74,7 @@ def constant_time_compare(val1, val2):
     if len(val1) != len(val2):
         return False
     result = 0
-    for x, y in zip(iter(val1), iter(val2)):
+    for x, y in zip(val1, val2):
         result |= x ^ y
     return result == 0
 
